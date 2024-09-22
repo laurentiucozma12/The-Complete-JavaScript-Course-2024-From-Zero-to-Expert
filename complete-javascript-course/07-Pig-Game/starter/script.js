60d98da4fa103654e05e7056ce6fb11045f9b496
Lau <laurentiucozma12@gmail.com>
2024-09-22 12:32:40 +0300

    changing player turn when dice hits 1

--- complete-javascript-course/07-Pig-Game/starter/script.js
+++ complete-javascript-course/07-Pig-Game/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const dice = document.querySelector('.dice');
+
+let sum = 0;
+let player = 1;
+
+document.querySelector('.btn--roll').addEventListener('click', event => {
+  let randomNumber = Math.trunc(Math.random() * 6 + 1);
+  sum += randomNumber;
+  console.log(randomNumber);
+
+  if (randomNumber === 1) {
+    changePlayerTurn();
+  }
+
+  if (player === 1) {
+    document.querySelector('#current--0').textContent = `${sum}`;
+    document.querySelector('.dice').src = `dice-${randomNumber}.png`;
+  } else {
+    document.querySelector('#current--1').textContent = `${sum}`;
+    document.querySelector('.dice').src = `dice-${randomNumber}.png`;
+  }
+});
+
+const changePlayerTurn = function () {
+  player++;
+  if (player === 2) {
+    player = 0;
+  }
+  return player;
+};
