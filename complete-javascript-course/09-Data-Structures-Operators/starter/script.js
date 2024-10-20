b03af4199b9947cf9c248244263a174550d6cf69
Lau <laurentiucozma12@gmail.com>
2024-10-20 10:39:14 +0300

    working with strings part 1 - done

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -44,10 +44,54 @@ const restaurant = {
   },
 };
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[0]); // A
+console.log(plane[1]); // 3
+console.log(plane[2]); // 2
+console.log(plane[3]); // 0
+console.log('B737'[0]); // B
+
+console.log(airline.length); // 16
+console.log('B737'.length); // 4
+
+console.log(airline.indexOf('r')); // 6
+console.log(airline.lastIndexOf('r')); // 10
+console.log(airline.lastIndexOf('Portugal')); // 8
+
+console.log(airline.slice(4)); // Air Portugal
+console.log(airline.slice(4, 7)); // Air
+
+console.log(airline.slice(0, airline.indexOf(' '))); // TAP
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // Portugal
+
+console.log(airline.slice(-2)); // al
+console.log(airline.slice(1, -1)); // AP Air Portuga
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  //// My solution
+  // if (seat.lastIndexOf('B') === -1 && seat.lastIndexOf('E') === -1) {
+  //   console.log(`Seat ${seat} is NOT a middle seat`);
+  // } else {
+  //   console.log(`Seat ${seat} is a middle seat`);
+  // }
+  //// His solution
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else {
+    console.log('You got lucky');
+  }
+};
+checkMiddleSeat('11B'); // Seat 11B is a middle seat
+checkMiddleSeat('23C'); // Seat 23C is NOT a middle seat
+checkMiddleSeat('3E'); // Seat 3E is a middle seat
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
 
 // Looping Objects: Object Keys, Values, and Entries
 
